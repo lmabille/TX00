5d206cad26a7a293b66d349bec4d215a9e8b4fb9
lmabille <mabille.l@icloud.com>
2022-05-03 19:10:31 +0200

    create script for each candidates

--- Snscrape/scripts/script_hashtag.sh
+++ Snscrape/scripts/script_hashtag.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-snscrape -n 100 --jsonl --with-entity --progress twitter-user JLMelenchon >JLMelenchon.json
-snscrape -n 100 --jsonl --with-entity --progress twitter-user JLMelenchon >JLMelenchon.json
 
+rm ./Snscrape/Output/*.json
 for i in Macron Lepen Poutou Zemmour Arthaud Dupont-Aignan Hidalgo Jadot Lassalle Mélenchon Pécresse Roussel
+#date jusque jour -1 pour le until
 do
-
- snscrape -n 100 --jsonl --with-entity --progress twitter-hashtag $i >./Output/${i}_hashtags.json
+ snscrape -n 1000 --jsonl --with-entity --progress twitter-search "since:2022-01-01 until:2022-04-11 #$i" >./Snscrape/Output/$i.json
 done
